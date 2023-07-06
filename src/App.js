3a91d8bf204104760986857d2f2ff319b55a7563
devil <devil@DevilSensai.netis.cc>
2023-07-06 09:59:55 +0545

    added code repo link

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import Header from './components/Header/Header';
 import Content from './components/Content/Content.jsx';
 import Personalized_product from './components/Personalized_product/Personalized_product';
 import Footer from './components/Footer/Footer';
-import './globle.css'
+import './globle.scss'
 function App() {
   return (
   <>
+      <div className="git_link"> 
+              <a href="http://www.github.com/kaushalbro/amazon"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="" /></a>
+    </div>
      <Header />
      <Content />
      <Personalized_product />
